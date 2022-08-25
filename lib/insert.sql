bc62d688fb8cf1b7b5450a4e955a12c9926d3e3a
Kelvin <vokemalosh32@gmail.com>
2022-08-25 21:31:07 +0300

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+
+
+INSERT INTO bears VALUES (1,"Pinky", 8, "F", "black", "carefree", 1);
+INSERT INTO bears VALUES (2,"Broom", 5, "F", "light", "Quiet", 0);
+INSERT INTO bears VALUES (3,"Rooky", 6, "M", "turn", "Calm", 0);
+INSERT INTO bears VALUES (4,"Boosty", 9, "F", "grey", "Fat", 0);
+INSERT INTO bears VALUES (5,"Gloom", 3, "M", "dark brown", "goofy", 0);
+INSERT INTO bears VALUES (6,"Pisky", 2, "F", "dim", "Grinchy", 1);
+INSERT INTO bears VALUES (7,"Kenya", 9, "M", "dark", "naive", 1);
+INSERT INTO bears VALUES (8,null, 8, "F", "brown", "peaceful", 0);
